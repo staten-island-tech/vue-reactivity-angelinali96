fa26791ab87557cb7d008ebae178ac415267041b
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-02-02 10:14:05 -0500

    initialize vue

---
+++ vue/src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 v-if="answer">correct</h1>
+    <h1 v-else>incorrect</h1>
+    <h3 v-for="animal in animals" :key="animal">{{ animal }}</h3>
+  </div>
+</template>
+
+<script setup>
+const answer = true;
+const animals = ["f", "g"];
+</script>
+
+<style lang="scss" scoped>
+
+</style>
