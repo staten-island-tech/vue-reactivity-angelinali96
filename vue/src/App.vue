a651cdb59a2257e187f8ff0af352d987b44c018f
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-02-05 10:13:53 -0500

    ???? .

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -16,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
+      
 
   <RouterView />
 </template>
---
+++ vue/src/components/DestCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>{{ destination.name }}</h2>
+        <img :src="destination.img" alt=""/>
+        <h3>{{ clicked }}</h3>
+        <button @click="increment">click</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props = defineProps({ // data expected to be received
+    destination: Object
+})
+// clicker logic
+const clicked = ref(0); // ref is number that can change
+function increment(){
+    clicked.value++;
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+img{
+    width: 250px;
+    object-fit: cover;
+}
+</style>
