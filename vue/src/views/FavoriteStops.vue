6373c6c4516565c8339ddebea72c2b368f128b58
haitang-blossoms <136626552+angelinali96@users.noreply.github.com>
2024-03-20 10:34:29 -0400

    start setup for favorites system
    not even close to done

--- vue/src/views/FavoriteStops.vue
+++ vue/src/views/FavoriteStops.vue
@@ -5,10 +5,11 @@
         <p>this will store saved stops do directly compare their bus/train times. </p>
         <p>didn't figure out how to store stuff in local mem yet</p>
     </div>
+<FavoriteButton/>
 </template>
 
 <script setup>
-
+import FavoriteButton from '@/components/FavoriteButton.vue';
 </script>
 
 <style scoped>
