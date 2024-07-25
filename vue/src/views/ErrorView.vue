e52dd02f9e9f9fd5ec8e515db756fe957af0a183
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-07-25 08:44:51 -0400

    fix errorpage display

--- vue/src/views/ErrorView.vue
+++ vue/src/views/ErrorView.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
+    <div style="display: flex; flex-direction: column; align-items: center;">
+        <Divider>
 <h1>Page not found</h1>
-<h2>404</h2>
-<RouterLink to="/">Go back to home page</RouterLink>
-    </div>
+</Divider>
+<Card style="width: 25rem; align-items: center; align-content: center;">
+    <template #title><span style="font-weight: 700;font-size: 8rem;">404</span></template>
+<template #content><RouterLink to="/">
+    <Button label="Go back to home" />
+</RouterLink></template>
+</Card>
+</div>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
+import Card from 'primevue/card';
 </script>
 
 <style scoped>
